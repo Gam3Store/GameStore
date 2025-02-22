54d337e6b6fb9c881af7cb43d4aae61be51d2aab
Gam3Store <gamestore.geugres@gmail.com>
2025-02-22 20:00:08 -0300

    Implementando o login

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:gamestore/pages/home_page.dart';
+import 'package:gamestore/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
